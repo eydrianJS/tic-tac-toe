1d5f2a2494c146a599551f06170af09fca6e289a
eydrianJS <adrian.olszowski@wp.pl>
2018-07-02 20:49:50 +0200

    basis apps, view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Announcement from'./Announcement';
+import ResetButton  from './ResetButton';
+import Tile from './Tile';
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      gameBoard: [
+        ' ',' ',' ',
+        ' ',' ',' ',
+        ' ',' ',' '
+      ],
+      turn: 'x'
+    }
+  }
+
+  updateBoard(loc, player) {
+
+  }
+
   render() {
-    return;
+    return(
+      <div className="container">
+        <div className="menu">
+          <h1>Tic-Tac_toe</h1>
+          <Announcement/>
+          <ResetButton/>          
+        </div>
+        {this.state.gameBoard.map(function(value, idx) {
+          return (
+            <Tile
+              key={idx}
+              loc={idx}
+              value={value}
+              updateBoard={this.updateBoard.bind(this)}
+              turn={this.state.turn}/>
+          )
+        }.bind(this))}
+      </div>
+    );
   }
 }
 
---
+++ src/ResetButton.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class ResetButton extends React.Component {
+
+    render() {
+        return (
+            <button onClick={this.props.reset}>Reset</button>
+        )
+    }
+}
